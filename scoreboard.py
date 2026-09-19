99a910fbb7f688621e5e5698ff5c409a020c1c28
dima.borovsky <dima.borovsky@imperva.com>
2026-09-19 16:32:55 +0300

    adding highscore reading from file

--- scoreboard.py
+++ scoreboard.py
@@ -6,7 +6,8 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score  = 0
-        self.highscore = 0
+        with open("high_score.txt") as data:
+            self.highscore =int(data.read())
         self.color("white")
         self.penup()
         self.goto(0,270)
@@ -21,7 +22,8 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore =self.score
-
+            with open("high_score.txt",mode="w") as data:
+                data.write(f"{self.highscore}")
         self.score = 0
         self.update_score()
 
